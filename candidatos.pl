b1a7dd2dad56edc192cc6d3f75af870398c5c570
Luis Astorga <luis.astorgaesquivel@gmail.com>
2026-05-26 14:46:45 -0600

    candidatos

--- candidatos.pl
+++ candidatos.pl
@@ -1,13 +1,17 @@
-% Hechos candidato/4 con nombre, habilidades técnicas, idiomas y experiencia (años) como listas.
+% Hechos candidato/4 con nombre, habilidades tï¿½cnicas, idiomas y experiencia (aï¿½os) como listas.
 
-% candidato(Id, Nombre, Habilidades, Idiomas)
+candidato(Id, Nombre, Habilidades, Idiomas)
+.
 
-candidato(c01, ana_torres,
-    [python, sql, mongodb, rest_api, git],
-    [espanol, ingles]).
-candidato(c02, luis_mendez,
-    [java, sql, spring_boot, docker, git],
-    [espanol]).
-candidato(c03, sofia_reyes,
-    [python, r, machine_learning, sql, tableau],
-    [espanol, ingles, frances]).
+candidato(c01, ana_torres,[python, sql, mongodb, rest_api, git],[espanol, ingles]).
+candidato(c02, luis_mendez,[java, sql, spring_boot, docker, git],[espanol]).
+candidato(c03, sofia_reyes,[python, r, machine_learning, sql, tableau],[espanol, ingles, frances]).
+candidato(c04, luis_astorga,[java, mongodb, rest_api, sprint_boot],[espanol, ingles]).
+candidato(c05, edson_lozano,[java, sql, spring_boot, docker, git],[espanol]).
+candidato(c06, brian_hernandez,[python, react, machine_learning, mongodb, tableau],[espanol, ingles, aleman]).
+candidato(c07, jesus_alberto,[python, sql, mongodb, rest_api, git],[espanol, ingles]).
+candidato(c08, miguel_perez,[java, sql, spring_boot, docker, git],[espanol]).
+candidato(c09, juan_reyes,[python, r, machine_learning, sql, tableau],[ingles, frances]).
+candidato(c10, carlos_garcia,[java, mongodb, rest_api, sprint_boot],[espanol, ingles]).
+candidato(c11, sofia_lopez,[python, sql, mongodb, rest_api, git],[espanol, ingles]).
+
