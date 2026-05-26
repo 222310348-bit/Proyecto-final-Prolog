f8c6e98c20b8694c4f070db2f937faa72b9de61c
Edson <222310348@itslerdo.edu.mx>
2026-05-26 14:30:53 -0600

    candidatos redaccion

--- candidatos.pl
+++ candidatos.pl
@@ -1,2 +1,13 @@
 % Hechos candidato/4 con nombre, habilidades técnicas, idiomas y experiencia (años) como listas.
 
+% candidato(Id, Nombre, Habilidades, Idiomas)
+
+candidato(c01, ana_torres,
+    [python, sql, mongodb, rest_api, git],
+    [espanol, ingles]).
+candidato(c02, luis_mendez,
+    [java, sql, spring_boot, docker, git],
+    [espanol]).
+candidato(c03, sofia_reyes,
+    [python, r, machine_learning, sql, tableau],
+    [espanol, ingles, frances]).
