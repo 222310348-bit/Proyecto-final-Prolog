7e1c0228dbbd004c933b70069f460ce5018155b4
Edson <222310348@itslerdo.edu.mx>
2026-05-26 15:32:42 -0600

    Insercion de funcion para verificar requisitos obligatorios

--- compatibilidad.pl
+++ compatibilidad.pl
@@ -1,2 +1,9 @@
 % Predicados cumple_requisitos/2, puntaje/3 y habilidades_faltantes/3.
 
+
+% Verifica requisitos obligatorios
+cumple_requisitos(CandId, VacId) :-
+    candidato(CandId, _, Habs, Idiomas),
+    vacante(VacId, Obligatorios, _),
+    append(Habs, Idiomas, Todo),
+    subset(Obligatorios, Todo).
