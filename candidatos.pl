8d388e3179b29620c0b97436c0d42a699b1d1a82
Edson <222310348@itslerdo.edu.mx>
2026-05-26 15:12:27 -0600

    Hechos de vacantes y correcion hechos candidatos

--- candidatos.pl
+++ candidatos.pl
@@ -1,7 +1,6 @@
 % Hechos candidato/4 con nombre, habilidades t�cnicas, idiomas y experiencia (a�os) como listas.
 
-candidato(Id, Nombre, Habilidades, Idiomas)
-.
+candidato(Id, Nombre, Habilidades, Idiomas).
 
 candidato(c01, ana_torres,[python, sql, mongodb, rest_api, git],[espanol, ingles]).
 candidato(c02, luis_mendez,[java, sql, spring_boot, docker, git],[espanol]).
@@ -13,5 +12,3 @@ candidato(c07, jesus_alberto,[python, sql, mongodb, rest_api, git],[espanol, ing
 candidato(c08, miguel_perez,[java, sql, spring_boot, docker, git],[espanol]).
 candidato(c09, juan_reyes,[python, r, machine_learning, sql, tableau],[ingles, frances]).
 candidato(c10, carlos_garcia,[java, mongodb, rest_api, sprint_boot],[espanol, ingles]).
-candidato(c11, sofia_lopez,[python, sql, mongodb, rest_api, git],[espanol, ingles]).
-
